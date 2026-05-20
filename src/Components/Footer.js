52755726e35b2e4d004d4ecb2efa53edc9ec9f5d
Neeraj Choithwani <neerajchoithwani28@gmail.com>
2026-05-20 12:51:56 +0530

    Initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">.</div>
+    </footer>
+  )
+}
+
+export default Footer
